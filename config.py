fb3f85e93277c36c806950631d4c5d5256ba7da9
AYAN MANNA <ayanmanna2019@gmail.com>
2026-09-25 17:09:30 +0530

    fixed

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
-load_dotenv()
+load_dotenv(override=True)
 
 class Config:
     FLASK_ENV = os.getenv('FLASK_ENV', 'development')
